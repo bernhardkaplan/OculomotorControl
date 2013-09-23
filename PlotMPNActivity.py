sign error in BG fixed dummy pursuit

PlotMPNActivity.py
@@ -37,11 +37,12 @@ class ActivityPlotter(object):
         d = np.zeros((self.it_max, self.x_grid.size)) #self.x_grid.size, self.it_max))
 
         for iteration in xrange(self.it_max):
+            print 'Plot input iteration', iteration
             fn_to_match = (self.params['input_nspikes_fn_mpn'] + 'it%d' % iteration).rsplit('/')[-1]
             list_of_files = utils.find_files(self.params['input_folder_mpn'], fn_to_match)
             for fn_ in list_of_files:
                 fn = self.params['input_folder_mpn'] + fn_
-                print 'Loading:', fn
+#                print 'Loading:', fn
                 d_it = np.loadtxt(fn, dtype=np.int)
                 nspikes = d_it[:, 1]
 
@@ -70,7 +71,13 @@ class ActivityPlotter(object):
         xlabels = ['%.1f' % (float(xtick) / self.n_bins_x) for xtick in self.x_ticks]
         ax.set_xticks(self.x_ticks)
         ax.set_xticklabels(xlabels)
+        output_fn = self.params['data_folder'] + 'mpn_input_activity.dat'
+        print 'Saving data to:', output_fn
+        np.savetxt(output_fn, d)
 
+        output_fig = self.params['figures_folder'] + 'mpn_input_activity.png'
+        print 'Saving figure to:', output_fn
+        pylab.savefig(output_fig)
 
     
 #    def get_nspikes(self, merged_spike_fn):
@@ -103,6 +110,7 @@ class ActivityPlotter(object):
         d = np.zeros((self.it_max, self.x_grid.size)) #self.x_grid.size, self.it_max))
         nspikes_thresh = 1
         for iteration in xrange(self.it_max):
+            print 'Plot output iteration', iteration
             cells_per_grid_cell = np.zeros(self.x_grid.size) # how many cells have been above a threshold activity during this iteration
             t0, t1 = iteration * self.params['t_iteration'], (iteration + 1) * self.params['t_iteration']
             nspikes = self.get_nspikes_interval(spike_data, t0, t1) 
@@ -131,6 +139,13 @@ class ActivityPlotter(object):
         xlabels = ['%.1f' % (float(xtick) / self.n_bins_x) for xtick in self.x_ticks]
         ax.set_xticks(self.x_ticks)
         ax.set_xticklabels(xlabels)
+        output_fn = self.params['data_folder'] + 'mpn_output_activity.dat'
+        print 'Saving data to:', output_fn
+        np.savetxt(output_fn, d)
+
+        output_fig = self.params['figures_folder'] + 'mpn_output_activity.png'
+        print 'Saving figure to:', output_fn
+        pylab.savefig(output_fig)
 
 
 

VisualInput.py
@@ -172,12 +172,12 @@ class VisualInput(object):
         time_axis = np.arange(0, t_integrate, self.params['dt_input_mpn'])
         # update the motion parameters based on the action
         if action_code[0] != None:
-            self.current_motion_params[0] += action_code[0] # shift x-position
+            self.current_motion_params[0] -= action_code[0] # shift x-position
             self.current_motion_params[2] = action_code[0]  # update v_stim_x
         else:
             self.current_motion_params = list(self.params['initial_state'])
         if (self.params['n_grid_dimensions'] == 2) and (action_code[1] != None):
-            self.current_motion_params[1] += action_code[1] # shift y-position
+            self.current_motion_params[1] -= action_code[1] # shift y-position
             self.current_motion_params[2] = action_code[1]  # update v_stim_y
 
         x_stim = self.current_motion_params[2] * time_axis / self.params['t_cross_visual_field'] + np.ones(time_axis.size) * self.current_motion_params[0]

main.py
@@ -8,6 +8,7 @@ import simulation_parameters
 import CreateConnections
 import nest
 import numpy as np
+import time
 
 def save_spike_trains(params, iteration, stim_list):
     n_units = len(stim_list)
@@ -32,6 +33,7 @@ if __name__ == '__main__':
         GP = simulation_parameters.global_parameters()
         GP.write_parameters_to_file() # write_parameters_to_file MUST be called before every simulation
         params = GP.params
+    t0 = time.time()
 
     VI = VisualInput.VisualInput(params)
     MT = MotionPrediction.MotionPrediction(params, VI)
@@ -60,5 +62,9 @@ if __name__ == '__main__':
         print 'Iteration: %d\tState after action: ' % (iteration), next_state
 #        VI.update_retina_image(BG.get_eye_direction())
 
-    np.savetxt(params['actions_taken_fn'], actions)
-    np.savetxt(params['network_states_fn'], network_states_net_net)
+    if pc_id == 0:
+        np.savetxt(params['actions_taken_fn'], actions)
+        np.savetxt(params['network_states_fn'], network_states_net_net)
+    t1 = time.time() - t0
+    print 'Time: %d [sec]' % t1
+