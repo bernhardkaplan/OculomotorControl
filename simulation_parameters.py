0a4f1b101e2c3cc47a5cc4c991bdb534c1311ae3
Bernhard Kaplan <bernhard.kaplan@gmail.com>
2014-06-13 14:31:04 +0200

    merge milner params

--- simulation_parameters.py
+++ simulation_parameters.py
@@ -39,25 +39,22 @@ class global_parameters(ParameterContainer.ParameterContainer):
         # ######################
         # SIMULATION PARAMETERS
         # ######################
-        self.params['Cluster'] = False
-        self.params['Cluster_Milner'] = False
+        self.params['Cluster'] = True
+        self.params['Cluster_Milner'] = True
         self.params['total_num_virtual_procs'] = 8
         if self.params['Cluster'] or self.params['Cluster_Milner']:
-            self.params['total_num_virtual_procs'] = 160
-#        self.params['n_rf'] = 50
-#        self.params['n_v'] = 40
-        self.params['n_rf'] = 20
-        self.params['n_v'] = 10
+            self.params['total_num_virtual_procs'] = 80
+        self.params['n_rf'] = 50
+        self.params['n_v'] = 40
+#        self.params['n_rf'] = 20
+#        self.params['n_v'] = 10
 
         self.params['training'] = True
         self.params['reward_based_learning'] = True
+        self.params['n_training_cycles'] = 3            # how often each stimulus is presented during training
+        self.params['n_training_x'] = 40 # number of training samples to cover the x-direction of the tuning space
+        self.params['n_training_v'] = 30 # number of training samples to cover the v-direction of the tuning space
 
-        self.params['n_training_cycles'] = 2            # how often each stimulus is presented during training
-#        if self.params['reward_based_learning']:
-#            assert (self.params['n_training_cycles'] % 2) == 0, 'Each stimulus needs to be presented twice (once with plasiticity off to get the reward signal, \
-#                    once with plasticity on when reward signal has arrived and the efference copy re-activating stimulus and D1/D2 activity'
-        self.params['n_training_x'] = 1 # number of training samples to cover the x-direction of the tuning space
-        self.params['n_training_v'] = 1 # number of training samples to cover the v-direction of the tuning space
         self.params['n_training_stim_per_cycle'] = self.params['n_training_x'] * self.params['n_training_v']
         self.params['n_stim_training'] = self.params['n_training_cycles'] * self.params['n_training_stim_per_cycle'] # total number of stimuli presented during training
 
@@ -68,7 +65,7 @@ class global_parameters(ParameterContainer.ParameterContainer):
             self.params['n_stim_testing'] = len(self.params['test_stim_range'])
         else:
             self.params['n_stim_testing'] = 1
-        self.params['t_iteration'] = 25.   # [ms] stimulus integration time, after this time the input stimulus will be transformed
+        self.params['t_iteration'] = 15.   # [ms] stimulus integration time, after this time the input stimulus will be transformed
         self.params['n_silent_iterations'] = 3 # for 2 silent iterations this should be 3
         if self.params['training']:
             if self.params['reward_based_learning']:
@@ -101,7 +98,7 @@ class global_parameters(ParameterContainer.ParameterContainer):
             self.params['n_stim'] = self.params['n_stim_training']
         else:
             self.params['n_stim'] = self.params['n_stim_testing']
-        self.params['n_iterations'] = (self.params['n_iterations_per_stim']) * self.params['n_stim'] # [ms] total simulation time 
+        self.params['n_iterations'] = self.params['n_stim'] * self.params['n_iterations_per_stim']
         self.params['t_sim'] = (self.params['n_iterations_per_stim']) * self.params['t_iteration'] * self.params['n_stim'] # [ms] total simulation time 
         self.params['dt'] = 0.1            # [ms] simulation time step
         self.params['dt_input_mpn'] = 0.1  # [ms] time step for the inhomogenous Poisson process for input spike train generation
