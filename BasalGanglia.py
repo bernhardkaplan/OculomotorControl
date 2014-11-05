d9be26c4c8b5ba9aed97e68eefefc1420dac1974
Bernhard Kaplan <bernhard.kaplan@gmail.com>
2014-11-05 18:10:23 +0100

    BUGFIX in BG for mpi.send

--- BasalGanglia.py
+++ BasalGanglia.py
@@ -575,7 +575,7 @@ class BasalGanglia(object):
                 prob_distr = utils.softmax(nspikes_by_action, T=self.params['softmax_action_selection_temperature'])
                 root_chosen_action = utils.draw_from_discrete_distribution(prob_distr, size=1)[0]
                 sampled_action_idx = utils.draw_from_discrete_distribution(prob_distr, size=1)[0]
-                for pid in xrange(self.n_proc):
+                for pid in xrange(1, self.n_proc): # do not send to your own node! 
                     self.comm.send(sampled_action_idx, pid, tag=self.iteration)
 #                print 'DEBUG BG.get_action_softmax: prob_distr:', prob_distr, ' sampled action:', sampled_action_idx
             else:
