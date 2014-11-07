e0c70124f3dc61ebad2873c8a18e554334378cd5
Bernhard Kaplan <bernhard.kaplan@gmail.com>
2014-11-07 13:02:03 +0100

    check bug in softmax action selection

--- simulation_parameters.py
+++ simulation_parameters.py
@@ -39,7 +39,7 @@ class global_parameters(ParameterContainer.ParameterContainer):
         # ######################
         # SIMULATION PARAMETERS
         # ######################
-        self.params['Cluster'] = True
+        self.params['Cluster'] = False
         self.params['Cluster_Milner'] = False
         self.params['total_num_virtual_procs'] = 8
         if self.params['Cluster'] or self.params['Cluster_Milner']:
