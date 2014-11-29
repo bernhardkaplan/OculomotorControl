9829bed79e19fa8137e21a67f7fa54e2f012a5bf
Bernhard Kaplan <bernhard.kaplan@gmail.com>
2014-11-29 16:04:29 +0100

    UPDATE: non-binary / gaussian reward scheme

--- run_analysis_and_plotting_scripts.py
+++ run_analysis_and_plotting_scripts.py
@@ -8,13 +8,17 @@ from PlottingScripts.PlotTesting import PlotTesting
 
 if __name__ == '__main__':
     print 'Running analysis...'
-    params = utils.load_params(sys.argv[1])
-    if params['training']:
-        P = PlotEverything(sys.argv, verbose=True)
-    else:
-        P = PlotTesting(sys.argv)
-    run_plot_bg(sys.argv, None)
-    MAC = MetaAnalysisClass([params['folder_name']])
-    n_stim = params['n_stim']
-    MAC = MetaAnalysisClass(['dummy', params['folder_name'], str(0), str(n_stim)])
+
+    for folder in sys.argv[1:]:
+
+        params = utils.load_params(folder)
+#        params = utils.load_params(sys.argv[1])
+        if params['training']:
+            P = PlotEverything(sys.argv, verbose=True)
+        else:
+            P = PlotTesting(sys.argv)
+        run_plot_bg(sys.argv, None)
+        MAC = MetaAnalysisClass([params['folder_name']])
+        n_stim = params['n_stim']
+        MAC = MetaAnalysisClass(['dummy', params['folder_name'], str(0), str(n_stim)])
 
